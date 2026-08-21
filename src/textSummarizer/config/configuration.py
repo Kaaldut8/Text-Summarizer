798d1b46ebb49f235b112031939035ee20866541
Kaaldut8 <raghavmaheshwari0803@gmail.com>
2026-08-22 00:07:32 +0530

    Data Transformation Notebook Added

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -49,4 +49,15 @@ class ConfigurationManager:
 
 
 
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+            config = self.config.data_transformation
     
+            create_directories([config.root_dir])
+    
+            data_transformation_config = DataTransformationConfig(
+                root_dir=config.root_dir,
+                data_path=config.data_path,
+                tokenizer_name = config.tokenizer_name
+            )
+    
+            return data_transformation_config
