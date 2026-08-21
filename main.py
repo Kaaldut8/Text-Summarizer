a800de8b835d9699b2c55b3b986d5d40e5983cf5
Kaaldut8 <raghavmaheshwari0803@gmail.com>
2026-08-22 00:04:58 +0530

    Data Validation Notebook Added

--- main.py
+++ main.py
@@ -26,16 +26,3 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-
-
-
-STAGE_NAME = "Data Transformation stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_transformation = DataTransformationTrainingPipeline()
-   data_transformation.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
