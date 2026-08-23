c387861a889e82de26d650194ed9d4633ac0a153
Kaaldut8 <raghavmaheshwari0803@gmail.com>
2026-08-23 14:10:24 +0530

    Data Transformation Notebook Added

--- src/textSummarizer/components/data_transformation.py
+++ src/textSummarizer/components/data_transformation.py
@@ -15,7 +15,7 @@ class DataTransformation:
 
     
     def convert_examples_to_features(self,example_batch):
-        input_encodings = self.tokenizer(example_batch['dialogue'] , max_length = 1024, truncation = True )
+        input_encodings = self.tokenizer(text_target=example_batch['dialogue'] , max_length = 1024, truncation = True )
         
         target_encodings = self.tokenizer(example_batch['summary'], max_length = 128, truncation = True )
             
